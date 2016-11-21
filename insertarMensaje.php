b0a9768a6d4c53e667cf283c31b8047b93674b96
Diego Garro Fonseca <diegogarro1994@gmail.com>
2016-11-21 16:03:17 -0600

    Archivos php

---
+++ insertarMensaje.php
@@ -0,0 +1,19 @@
+<?php 
+	require("cone.php");
+	/*$user=$_POST["usuario"];
+	$password=md5($_POST["password"]);
+	$nomb=$_POST["nombre"];
+	$carn=$_POST["carnet"];
+	$corr=$_POST["correo"];
+
+	$sql = "INSERT INTO tbUsuario (usuario,pass,nombre,carnet,correo)
+	VALUES ('$user','$password','$nomb','$carn','$corr')";
+
+	if ($conn->query($sql) === TRUE) {
+    echo "Correcto";
+	} else {
+    echo "Incorrecto";
+	}
+	$conn->close();*/
+
+?>
---
+++ loguear.php
@@ -0,0 +1,43 @@
+<?php
+
+	require("cone.php");
+	$usuario=$_POST["user_name"];
+	$pass=md5($_POST["password"]);
+$result = $conn->query("SELECT nombre,correo FROM tbUsuario where usuario='$usuario' and pass='$pass'" );
+  
+	if($conn->affected_rows>0)
+	{
+		$datos="";
+		while($row = mysqli_fetch_array($result)){
+        	$datos=$datos.$row["nombre"]."#";
+        	$datos=$datos.$row["correo"]."*";
+        }
+		$result = $conn->query("SELECT u.nombre,u.correo, m.mensaje FROM tbUsuario u,tbMensajes m where u.nombre =m.destinatario and u. usuario='$usuario' and u.pass='$pass' ");
+  
+	if($conn->affected_rows>0)
+	{
+		$datos="";
+		$cont=0;
+		while($row = mysqli_fetch_array($result)){
+			if($cont<1)
+			{
+			$datos=$datos.$row["nombre"]."#";
+        	$datos=$datos.$row["correo"]."*";
+        	$cont=$cont+1;
+			}else
+			{
+			$datos=$datos.$row["mensaje"]."~";
+			}
+        }
+	}
+	else {
+	//echo "";
+	}
+
+	echo $datos;
+	}
+	else {
+	echo "Usuario Incorrecto";
+	}
+	
+?>
