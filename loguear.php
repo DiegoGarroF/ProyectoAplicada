bce03232a336ee0d08ab7099dc66b545b72d1efd
Luis González <luis0270@gmail.com>
2016-11-23 10:00:47 -0600

    archivo modificado con xd

--- loguear.php
+++ loguear.php
@@ -4,7 +4,7 @@
 	$usuario=$_POST["user_name"];
 	$pass=md5($_POST["password"]);
 $result = $conn->query("SELECT nombre,correo FROM tbUsuario where usuario='$usuario' and pass='$pass'" );
-  
+
 	if($conn->affected_rows>0)
 	{
 		$datos="";
@@ -13,7 +13,7 @@ $result = $conn->query("SELECT nombre,correo FROM tbUsuario where usuario='$usua
         	$datos=$datos.$row["correo"]."*";
         }
 		$result = $conn->query("SELECT u.nombre,u.correo, m.mensaje FROM tbUsuario u,tbMensajes m where u.nombre =m.destinatario and u. usuario='$usuario' and u.pass='$pass' ");
-  
+
 	if($conn->affected_rows>0)
 	{
 		$datos="";
@@ -22,7 +22,8 @@ $result = $conn->query("SELECT nombre,correo FROM tbUsuario where usuario='$usua
 			if($cont<1)
 			{
 			$datos=$datos.$row["nombre"]."#";
-        	$datos=$datos.$row["correo"]."*";
+      $datos=$datos.$row["correo"]."*";
+			$datos=$datos.$row["mensaje"]."~";
         	$cont=$cont+1;
 			}else
 			{
@@ -39,5 +40,5 @@ $result = $conn->query("SELECT nombre,correo FROM tbUsuario where usuario='$usua
 	else {
 	echo "Usuario Incorrecto";
 	}
-	
+
 ?>
