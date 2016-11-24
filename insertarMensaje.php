1181197e6a73a58d456c22e14a3d6b9747647a52
Diego Garro Fonseca <diegogarro1994@gmail.com>
2016-11-23 22:21:59 -0600

    Modificacion de los archivos

--- insertarMensaje.php
+++ insertarMensaje.php
@@ -1,19 +1,22 @@
 <?php 
-	require("cone.php");
-	/*$user=$_POST["usuario"];
-	$password=md5($_POST["password"]);
-	$nomb=$_POST["nombre"];
-	$carn=$_POST["carnet"];
-	$corr=$_POST["correo"];
 
-	$sql = "INSERT INTO tbUsuario (usuario,pass,nombre,carnet,correo)
-	VALUES ('$user','$password','$nomb','$carn','$corr')";
+	// NOTA ::::: EL remitente(int idUsario) que le llegue a este POST tiene existir en la tabla usuario
+	// y el destinatario igualmente(nombre del usuario en la tbUsuario) tiene que existir en tbUsuario.. aqui no se hizo con ID de usuario xq ya estaba hecho con el nombre entonces habia que cambiar codigo en algunas partes, y como el proyecto es un muestra nada mas, entonces no nos enfocamos mucho en integridad referencial:::::
+
+	require("cone.php");
+	$remitente=$_POST["remitente"];
+	$destinatario=$_POST["destinatario"];
+	$mensaje=$_POST["mensaje"];
+	$estado=$_POST["estado"];
+
+	$sql = "INSERT INTO tbMensajes (remitente,destinatario,mensaje,estado)
+	VALUES ('$remitente','$destinatario','$mensaje','$estado')";
 
 	if ($conn->query($sql) === TRUE) {
     echo "Correcto";
 	} else {
     echo "Incorrecto";
 	}
-	$conn->close();*/
+	$conn->close();
 
 ?>
--- loguear.php
+++ loguear.php
@@ -3,16 +3,17 @@
 	require("cone.php");
 	$usuario=$_POST["user_name"];
 	$pass=md5($_POST["password"]);
+
 $result = $conn->query("SELECT nombre,correo FROM tbUsuario where usuario='$usuario' and pass='$pass'" );
 
 	if($conn->affected_rows>0)
 	{
 		$datos="";
 		while($row = mysqli_fetch_array($result)){
-        	$datos=$datos.$row["nombre"]."#";
-        	$datos=$datos.$row["correo"]."*";
+        	$datos=$datos.$row["nombre"]."##";
+        	$datos=$datos.$row["correo"]."##";
         }
-		$result = $conn->query("SELECT u.nombre,u.correo, m.mensaje FROM tbUsuario u,tbMensajes m where u.nombre =m.destinatario and u. usuario='$usuario' and u.pass='$pass' ");
+		$result = $conn->query("SELECT u.nombre,u.correo, m.mensaje,m.remitente,m.fecha FROM tbUsuario u,tbMensajes m where u.usuario =m.destinatario and u. usuario='$usuario' and u.pass='$pass' ");
 
 	if($conn->affected_rows>0)
 	{
@@ -21,16 +22,20 @@ $result = $conn->query("SELECT nombre,correo FROM tbUsuario where usuario='$usua
 		while($row = mysqli_fetch_array($result)){
 			if($cont<1)
 			{
-			$datos=$datos.$row["nombre"]."#";
-      $datos=$datos.$row["correo"]."*";
-			$datos=$datos.$row["mensaje"]."~";
+			$datos=$datos.$row["nombre"]."##";
+            $datos=$datos.$row["correo"]."##";
+			$datos=$datos.$row["mensaje"].  "///////////";
+			$datos=$datos.$row["remitente"]."///////////";
+			$datos=$datos.$row["fecha"].    "///////////";
         	$cont=$cont+1;
 			}else
 			{
-			$datos=$datos.$row["mensaje"]."~";
+			$datos=$datos.$row["mensaje"].   "///////////";
+			$datos=$datos.$row["remitente"]."///////////";
+			$datos=$datos.$row["fecha"].   "///////////";
 			}
         }
-	}
+	}// fin de los mensajes
 	else {
 	//echo "";
 	}
@@ -38,6 +43,7 @@ $result = $conn->query("SELECT nombre,correo FROM tbUsuario where usuario='$usua
 	echo $datos;
 	}
 	else {
+		
 	echo "Usuario Incorrecto";
 	}
 
